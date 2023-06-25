d867d50435091be0abd954c0fdb62bf45ecc775f
R1chard <esanrichard1@gmail.com>
2023-06-25 23:39:03 +0100

    update

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
 import Signinimg from "../../public/assets/images/uni.jpg";
+import Unimart from "../../public/assets/svgicons/unimart.svg";
+
 const Login = () => {
   return (
     <section className="loginpage">
       <div className="logininfo">
+        <Image src={Unimart} alt={Unimart} className="signuplogo" />
         <div className="mainloginifo">
         <h2 className="signintext">Sign in</h2>
         <p className="link-text">
@@ -16,7 +19,7 @@ const Login = () => {
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
         <p className="forgotpassword">
-          <Link href="/forgotpassword">Forgot Password</Link>
+          <Link href="/forgotpassword">Forgot Password?</Link>
         </p>
         <div className="signin-btn">Sign in</div>
 
