6f775baec3dbbd15883ac6b27b2adf54ac4a14d5
R1chard <esanrichard1@gmail.com>
2023-06-25 21:56:30 +0100

    update

---
+++ app/forgotpassword/page.jsx
@@ -0,0 +1,8 @@
+
+const page = () => {
+  return (
+    <div>forgot password</div>
+  )
+}
+
+export default page
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,7 +1,16 @@
-
+import Link from "next/link"
 const Login = () => {
   return (
-    <div>Login</div>
+    <section>
+      <h2>Sign in</h2>
+      <p>New to Unimart <Link href="/signup"><span>Sign up for free</span></Link></p>
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password"/> 
+      <p><Link href="/forgotpassword">Forgot Password</Link></p>
+      <div className="signin-btn">
+        Sign in
+      </div>
+    </section>
   )
 }
 
