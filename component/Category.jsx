dc6a4d84c30e063e8fa5cf51644f9e511255fd2a
R1chard <esanrichard1@gmail.com>
2023-06-25 20:45:16 +0100

    update

--- component/Category.jsx
+++ component/Category.jsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import mobile from "../public/assets/svgicons/mobile.svg";
-
 import electronics from "../public/assets/svgicons/electronics.svg";
 import funiture from "../public/assets/svgicons/funiture.svg";
 import beauty from "../public/assets/svgicons/beauty.svg";
@@ -13,12 +12,13 @@ const Category = () => {
   return (
     <section className="category-container">
       <div className="homepage-category">
-        <Image src={mobile} className="mobile"alt="mobile" />
-        <Image src={electronics} className="electronics" alt="electronics" />
-        <Image src={funiture} className="furniture" alt="funiture" />
-        <Image src={beauty} className="beauty" alt="beauty" />
-        <Image src={pets} className="pets" alt="pets" />
-        <Image src={mobile} className="mobile" alt="mobile" />
+        <Image src={mobile} className="mobile home-cat"alt="mobile" />
+        <Image src={electronics} className="electronics home-cat" alt="electronics" />
+        {/* <Image src={funiture} className="furniture home-cat" alt="funiture" /> */}
+        <Image src={beauty} className="beauty home-cat" alt="beauty" />
+        <Image src={pets} className="pets home-cat" alt="pets" />
+        <Image src={mobile} className="mobile home-cat" alt="mobile" />
+        <Image src={mobile} className="mobile home-cat" alt="mobile" />
         <Image src={left}  alt="phone" className="leftcategory" />
         <Image src={right}  alt="phone" className="rightcategory" />
       </div>
