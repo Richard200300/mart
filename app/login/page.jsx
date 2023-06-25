3a7dd48be5a060d533cbcbf71e27106dca2d01c2
R1chard <esanrichard1@gmail.com>
2023-06-25 23:11:03 +0100

    update

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,17 +1,33 @@
-import Link from "next/link"
+import Link from "next/link";
+import Image from "next/image";
+import Signinimg from "../../public/assets/images/uni.jpg";
 const Login = () => {
   return (
-    <section>
-      <h2>Sign in</h2>
-      <p>New to Unimart <Link href="/signup"><span>Sign up for free</span></Link></p>
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password"/> 
-      <p><Link href="/forgotpassword">Forgot Password</Link></p>
-      <div className="signin-btn">
-        Sign in
+    <section className="loginpage">
+      <div className="logininfo">
+        <div className="mainloginifo">
+        <h2 className="signintext">Sign in</h2>
+        <p className="link-text">
+          New to Unimart
+          <Link href="/signup">
+            <span className="su-link">Sign up for free</span>
+          </Link>
+        </p>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <p>
+          <Link href="/forgotpassword">Forgot Password</Link>
+        </p>
+        <div className="signin-btn">Sign in</div>
+
+        </div>
+        
+      </div>
+      <div className="loginimg">
+        <Image src={Signinimg} alt={Signinimg} className="Signinimg" />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
