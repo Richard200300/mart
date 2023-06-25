fd657cf1dd5147772222c5b405dea1d1d41d776c
R1chard <esanrichard1@gmail.com>
2023-06-25 04:13:19 +0100

    update

--- component/navbar.jsx
+++ component/navbar.jsx
@@ -1,12 +1,22 @@
+"use client"
 import Image from "next/image";
 import Unimart from "../public/assets/svgicons/unimart.svg";
 import unimarrtsearch from "../public/assets/svgicons/unimarrtsearch.svg";
 import signupicon from "../public/assets/svgicons/signupicon.svg";
 import Link from "next/link";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [tognav, setTognav] = useState(true)
+
   return (
     <section className="main-nav">
+      <div className={tognav ? "showmenu" : "showmenuactive"}>
+        <div className="mainshow">
+          
+        </div>
+
+      </div>
       <section className="navbar">
         <Image src={Unimart} alt="unimart" className="unimart-logo" />
 
@@ -22,10 +32,12 @@ const Navbar = () => {
             className="nav-search-icon"
           />
         </div>
-        <div className="hambugger-nav">
-          <div className="nav-bar"></div>
-          <div className="nav-bar"></div>
-          <div className="nav-bar"></div>
+        <div className="hambugger-nav" onClick={()=>{
+setTognav(prevdata => !prevdata)
+        }}>
+          <div className={tognav ? "nav-bar": "bar-active"}></div>
+          <div className={tognav ? "nav-bar": "bar-active"}></div>
+          <div className={tognav ? "nav-bar": "bar-active"}></div>
         </div>
         <div className="nav-logincont">
           <Link href="/login">
